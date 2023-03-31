f44183da56a5a2e3790f768b74e6f1e37976a169
songmuhan <809617759@qq.com>
2023-03-31 15:13:19 +0800

    lab6:ex4 mmio map pass

--- kern/e1000.c
+++ kern/e1000.c
@@ -1,8 +1,14 @@
 #include <kern/e1000.h>
 #include <kern/pmap.h>
 
+
+volatile uint32_t *E1000BASE;
+#define REG(offset) (E1000BASE[offset / 4])
+
 // LAB 6: Your driver code here
 int e1000_attach(struct pci_func *f){
     pci_func_enable(f);
+    E1000BASE = mmio_map_region(f->reg_base[0],f->reg_size[0]);
+    assert(REG(E1000_STATUS) == 0x80080783);
     return 0;
 }
--- kern/e1000.h
+++ kern/e1000.h
@@ -6,3 +6,4 @@
 int e1000_attach(struct pci_func *);
 
 
+#define E1000_STATUS   0x00008  /* Device Status - RO */
